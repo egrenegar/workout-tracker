d9abfe4f8d292b824ced5e74e3fc07b01cea45ea
Emily Renegar <egrenegar@gmail.com>
2020-06-24 17:50:10 -0400

    reorganized file structure

--- models/Workout.js
+++ models/Workout.js
@@ -3,17 +3,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: Date,
-    exercises: [
-        {
-          type: String,
-          name: String,
-          duration: Number,
-          weight: Number,
-          reps: Number,
-          sets: Number
-        }
-    ]
+  day: {
+    type: Date,
+    default: Date.now()
+  },
+  exercises: [
+    {
+      type: { type: String},
+      name: {type: String},
+      duration: { type: Number},
+      distance: {type: Number},
+      weight: {type: Number},
+      reps: {type: Number},
+      sets: {type: Number}
+    }
+  ]
+}, { toJSON: { virtuals: true } });
+
+WorkoutSchema.virtual('totalDuration').get(function(){
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
 const Workout = mongoose.model('Workout', WorkoutSchema);
