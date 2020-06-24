ac3a894344d77c64660f13b591d91c05e178324d
Emily Renegar <egrenegar@gmail.com>
2020-06-24 17:49:46 -0400

    fixed quotes issue in /api/workouts

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+const db = require('./models');
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true });
+
+app.get('/api/workouts', (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.post('/api/workouts', (req, res) => {
+  db.Workout.find({})
+  .then(dbWorkout => {
+
+  })
+})
+
+// app.get('/api/workouts/:id', (req, res) => {
+//   db.Workout.find({})
+//     .then(dbWorkout => {
+//       res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
