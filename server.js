aa4ea8e5df105f2d4eba6437940aafdec03daefd
Emily Renegar <egrenegar@gmail.com>
2020-06-24 19:19:13 -0400

    added the rest of the routes

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 const db = require('./models');
+const { Workout } = require('./models');
+
+// use if putting routes in separate folder
+// require('./routes/api-routes')(app);
+// require('./routes/html-routes')(app);
 
 const app = express();
 
@@ -17,6 +23,7 @@ app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true });
 
+//==========API ROUTES===========//
 app.get('/api/workouts', (req, res) => {
   db.Workout.find({})
     .then(dbWorkout => {
@@ -28,22 +35,49 @@ app.get('/api/workouts', (req, res) => {
 });
 
 app.post('/api/workouts', (req, res) => {
-  db.Workout.find({})
-  .then(dbWorkout => {
-
+  const workout = new Workout(req.body);
+  db.Workout.create(workout)
+  .then(Workout => {
+    res.json(Workout);
   })
+  .catch(err => {
+    res.json(err);
+  });
 })
 
-// app.get('/api/workouts/:id', (req, res) => {
-//   db.Workout.find({})
-//     .then(dbWorkout => {
-//       res.json(dbWorkout);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
+app.put("/api/workouts/:id", ({body}, res) => {
+  db.Workout.create(body)
+    .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
+app.get('/api/workouts/range', (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+//=========== HTML ROUTES ===========//
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/exercise.html'));
+});
+
+app.get('/stats', (req, res)=> {
+  res.sendFile(path.join(__dirname, './public/stats.html'));
+});
 
 // Start the server
 app.listen(PORT, () => {
