43e02e49e47507d5a546780d77316470a43940de
Emily Renegar <egrenegar@gmail.com>
2020-06-24 22:16:31 -0400

    reformatted model

--- models/Workout.js
+++ models/Workout.js
@@ -9,35 +9,13 @@ const WorkoutSchema = new Schema({
   },
   exercises: [
     {
-      type:
-      {
-        type: String,
-        required: 'Type of exercise is required.'
-      },
-      name:
-      {
-        type: String
-      },
-      duration:
-      {
-        type: Number
-      },
-      distance:
-      {
-        type: Number
-      },
-      weight:
-      {
-        type: Number
-      },
-      reps:
-      {
-        type: Number
-      },
-      sets:
-      {
-        type: Number
-      }
+      type: { type: String },
+      name: { type: String },
+      duration: { type: Number },
+      distance: { type: Number },
+      weight: { type: Number },
+      reps: { type: Number },
+      sets: { type: Number }
     }
   ]
 }, { toJSON: { virtuals: true } });
